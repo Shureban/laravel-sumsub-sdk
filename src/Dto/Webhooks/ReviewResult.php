0329f8edece0459fa02929477e105d2cb02c55d7
Shureban <Shureban@gmail.com>
2023-03-13 13:37:15 +0200

    add more fields in ReviewResult object

--- src/Dto/Webhooks/ReviewResult.php
+++ src/Dto/Webhooks/ReviewResult.php
@@ -2,9 +2,16 @@
 
 namespace Shureban\LaravelSumsubSdk\Dto\Webhooks;
 
+use Shureban\LaravelSumsubSdk\Enums\RejectLabel;
 use Shureban\LaravelSumsubSdk\Enums\ReviewAnswer;
+use Shureban\LaravelSumsubSdk\Enums\ReviewRejectType;
 
 class ReviewResult
 {
-    public ReviewAnswer $reviewAnswer;
+    /** @var RejectLabel[] */
+    public array             $rejectLabels     = [];
+    public ?ReviewRejectType $reviewRejectType = null;
+    public ReviewAnswer      $reviewAnswer;
+    public string            $moderationComment;
+    public string            $clientComment;
 }
