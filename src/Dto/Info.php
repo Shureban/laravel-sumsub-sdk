9eec8a58e15792ff8f19403e05941371b97a174d
Shureban <Shureban@gmail.com>
2024-01-17 13:56:52 +0200

    add more fields to info class

--- src/Dto/Info.php
+++ src/Dto/Info.php
@@ -2,12 +2,20 @@
 
 namespace Shureban\LaravelSumsubSdk\Dto;
 
+use DateTime;
 use Shureban\LaravelSumsubSdk\Enums\Country;
 
 class Info
 {
+    public ?string      $firstName;
+    public ?string      $firstNameEn;
+    public ?string      $lastName;
+    public ?string      $lastNameEn;
+    public ?DateTime    $dob;
     public ?CompanyInfo $companyInfo = null;
     public ?Country     $country;
+    /** @var Address[] */
+    public array $addresses = [];
     /** @var IdDoc[] */
     public array $idDocs = [];
 }
