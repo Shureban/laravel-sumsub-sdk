ce0133499646cbfb8758010edba1c155aaa8f05a
Shureban <Shureban@gmail.com>
2025-02-10 15:10:40 +0200

    remove reject labels

--- src/Dto/ReviewResult.php
+++ src/Dto/ReviewResult.php
@@ -2,13 +2,12 @@
 
 namespace Shureban\LaravelSumsubSdk\Dto;
 
-use Shureban\LaravelSumsubSdk\Enums\RejectLabel;
 use Shureban\LaravelSumsubSdk\Enums\ReviewAnswer;
 use Shureban\LaravelSumsubSdk\Enums\ReviewRejectType;
 
 class ReviewResult
 {
-    /** @var RejectLabel[] */
+    /** @var string[] */
     public array             $rejectLabels     = [];
     public ?ReviewRejectType $reviewRejectType = null;
     public ReviewAnswer      $reviewAnswer;
