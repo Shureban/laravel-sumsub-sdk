5f85afa18aee625a5afc9bcd6a35cdec7d7e33ca
Shureban <Shureban@gmail.com>
2025-03-17 08:55:54 +0000

    drop enums

--- src/Dto/DocSet.php
+++ src/Dto/DocSet.php
@@ -2,18 +2,16 @@
 
 namespace Shureban\LaravelSumsubSdk\Dto;
 
-use Shureban\LaravelSumsubSdk\Enums\DocSetType;
 use Shureban\LaravelSumsubSdk\Enums\DocSubType;
-use Shureban\LaravelSumsubSdk\Enums\IdDocType;
 
 class DocSet
 {
-    public DocSetType $idDocSetType;
+    public string $idDocSetType;
     /** @var DocSetField[] */
     public array $fields = [];
     /** @var DocSetField[] */
     public array $customFields = [];
-    /** @var IdDocType[] */
+    /** @var string[] */
     public array $types = [];
     /** @var DocSubType[] */
     public array   $subTypes      = [];
