04f4495e6d83769e089636cdf0a25e3e619965c5
Shureban <Shureban@gmail.com>
2025-10-21 15:11:16 +0300

    add company info in fixedInfo

--- src/Dto/FixedInfo.php
+++ src/Dto/FixedInfo.php
@@ -8,20 +8,21 @@ use Shureban\LaravelSumsubSdk\Enums\Gender;
 
 class FixedInfo
 {
-    public ?string   $firstName      = null;
-    public ?string   $lastName       = null;
-    public ?string   $middleName     = null;
-    public ?string   $firstNameEn    = null;
-    public ?string   $lastNameEn     = null;
-    public ?string   $middleNameEn   = null;
-    public ?string   $legalName      = null;
-    public ?Gender   $gender         = null;
-    public ?DateTime $dob            = null;
-    public ?string   $placeOfBirth   = null;
-    public ?string   $countryOfBirth = null;
-    public ?string   $stateOfBirth   = null;
-    public ?Country  $country        = null;
-    public ?Country  $nationality    = null;
+    public ?string      $firstName      = null;
+    public ?string      $lastName       = null;
+    public ?string      $middleName     = null;
+    public ?string      $firstNameEn    = null;
+    public ?string      $lastNameEn     = null;
+    public ?string      $middleNameEn   = null;
+    public ?string      $legalName      = null;
+    public ?Gender      $gender         = null;
+    public ?DateTime    $dob            = null;
+    public ?CompanyInfo $companyInfo    = null;
+    public ?string      $placeOfBirth   = null;
+    public ?string      $countryOfBirth = null;
+    public ?string      $stateOfBirth   = null;
+    public ?Country     $country        = null;
+    public ?Country     $nationality    = null;
     /** @var Address[] */
     public array   $addresses = [];
     public ?string $tin       = null;
