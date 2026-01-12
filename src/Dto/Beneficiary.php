2ecc8843a11d694caf6b4b53a0d84ec6d8256dcd
makeale <i.makarov@inxy.io>
2026-01-12 17:48:03 +0400

    update company beneficiary

--- src/Dto/Beneficiary.php
+++ src/Dto/Beneficiary.php
@@ -2,15 +2,16 @@
 
 namespace Shureban\LaravelSumsubSdk\Dto;
 
+use Shureban\LaravelSumsubSdk\Dto\Responses\ApplicantData;
 use Shureban\LaravelSumsubSdk\Enums\BeneficiaryType;
 
 class Beneficiary
 {
-    public string          $applicantId;
-    public ?array          $positions  = null;
-    public BeneficiaryType $type;
-    public bool            $inRegistry = false;
-    public ?array          $imageIds   = null;
-    public mixed           $applicant  = null;
-    public ?float          $shareSize  = null;
+    public string           $id;
+    public ?ApplicantData   $applicant;
+    public string           $applicantId;
+    /** @var BeneficiaryType[] */
+    public ?array           $types = null;
+    public ?float           $shareSize  = null;
+    public ?BeneficiaryInfo $beneficiaryInfo = null;
 }
